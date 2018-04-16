118d719599df0a6d09b160b47a7500835338fc67
MITZ1979 <1519792930@qq.com>
2018-04-17 07:32:48 +0800

    commit insert

--- application/index/view/teacher/teacher_list.php
+++ application/index/view/teacher/teacher_list.php
@@ -76,7 +76,6 @@
         {/volist}
         </tbody>
     </table>
-    <div id="pageNav" class="pageNav"></div>
 </div>
 <!--_footer 作为公共模版分离出去-->
 <script type="text/javascript" src="__STATIC__/lib/jquery/1.9.1/jquery.min.js"></script>
@@ -89,30 +88,6 @@
 <script type="text/javascript" src="__STATIC__/lib/datatables/1.10.0/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="__STATIC__/lib/laypage/1.2/laypage.js"></script>
 <script type="text/javascript">
-    window.onload = (function () {
-        // optional set
-        pageNav.pre = "&lt;上一页";
-        pageNav.next = "下一页&gt;";
-        // p,当前页码,pn,总页面
-        pageNav.fn = function (p, pn) {
-            $("#pageinfo").text("当前页:" + p + " 总页: " + pn);
-        };
-        //重写分页状态,跳到第三页,总页33页
-        pageNav.go(1, 13);
-    });
-//    $('.table-sort').dataTable({
-//        "lengthMenu": false,//显示数量选择
-//        "bFilter": false,//过滤功能
-//        "bPaginate": false,//翻页信息
-//        "bInfo": false,//数量信息
-//        "aaSorting": [[1, "desc"]],//默认第几个排序
-//        "bStateSave": true,//状态保存
-//        "aoColumnDefs": [
-//            //{"bVisible": false, "aTargets": [ 3 ]} //控制列的隐藏显示
-//            {"orderable": false, "aTargets": [0, 8, 9]}// 制定列不参与排序
-//        ]
-//    });
-
 //停用的方法
 function teach_stop(obj, id) {
     layer.confirm('确认要停用吗？', function (index) {
